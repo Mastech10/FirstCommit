0866591d0de9635840e03586346fc9fe6c53cf01
Mastech10 <mastechperu@gmail.com>
2018-05-27 03:27:05 -0500

    first commit

---
+++ src/app/app.firebaseconfig.ts
@@ -0,0 +1,8 @@
+export var config = {
+    apiKey: "AIzaSyDVpXh4WmzvdjosOvx8OybGIiPA5oY2tLc",
+    authDomain: "ionicapp-jmas.firebaseapp.com",
+    databaseURL: "https://ionicapp-jmas.firebaseio.com",
+    projectId: "ionicapp-jmas",
+    storageBucket: "ionicapp-jmas.appspot.com",
+    messagingSenderId: "922876077023" 
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,26 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { config } from './app.firebaseconfig';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
